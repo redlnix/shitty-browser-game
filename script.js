71d13e03706c49fcc4c44e79952518c1ddbbb4bc
Parker <tylerlecrater@gmail.com>
2025-12-07 07:50:39 -0500

    67

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-document.getElementById("daniel123")
-
 document.getElementById("daniel123").onclick = function() {
     alert("you clicked");
 }
-
-document.getElementById("thomas123")
 
 document.getElementById("thomas123").onclick = function() {
     document.body.classList.toggle("bomb");
