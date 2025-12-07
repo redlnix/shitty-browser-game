b39dcbed44b1f0148f2bf6157cc5d86b5247f56a
red <hopsread@gmail.com>
2025-12-06 22:33:24 -0500

    playing around with more buttons and css (also added spots for daniel)

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ document.getElementById("daniel123")
 document.getElementById("daniel123").onclick = function() {
     alert("you clicked");
 }
+
+document.getElementById("thomas123")
+
+document.getElementById("thomas123").onclick = function() {
+    
+}
