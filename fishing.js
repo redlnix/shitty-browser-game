261bbc7882a63a26ac3a2705adc90877d8894c78
Parker <tylerlecrater@gmail.com>
2025-12-07 20:32:21 -0500

    it works now

--- fishing.js
+++ fishing.js
@@ -1,15 +1,17 @@
 const bar = document.getElementById('fishing-fill')
+let progress = 0
 let Barheight = 70
 let BarY = 0
 let Mouse = false
 let MomentumY = 0
 let Fishheight = 50
-let FishY = 300
 let FishMomentum = 0
+let FishStats = {speedran: 4, speedbase: 4, timeran: 40, timebase: 30}
+
 
 function UpdateBar() {
 BarY = Math.min(BarY, 600 - Barheight)
-if (BarY <= 0) MomentumY = 0
+if (BarY <= 0) MomentumY = Math.abs(MomentumY * 0.8)
 BarY = Math.max(BarY, 0)
 
 bar.style.bottom = BarY + 'px'
@@ -34,43 +36,51 @@ MomentumY = Math.max(MomentumY, -8)
 BarY += MomentumY
 UpdateBar()
 
+UpdateFish()
+
+const Fish = document.getElementById('fish')
+
+const FishY = parseInt(Fish.style.bottom)
+
+if (FishY + Fishheight >= BarY && FishY <= BarY + Barheight) {
+console.log('touch')
+}
+
+}, 20)
 
 
-const fish = document.getElementById('fish')
+function UpdateFish() {
+const Fish = document.getElementById('fish')
 
-if (Number(fish.dataset.timer) < 1) {
+if (Number(Fish.dataset.timer) < 1) {
 
 let direction = -1
 if (Math.floor(Math.random() * 2) == 1) {
 direction = 1;
 }
-if (parseInt(fish.style.bottom) < 100) {
+if (parseInt(Fish.style.bottom) < 100) {
 direction = 1;
-} else if (parseInt(fish.style.bottom) > 500) {
+} else if (parseInt(Fish.style.bottom) > 500) {
 direction = -1;
 }
 
-fish.dataset.momentum = (Math.random() * 4 + 4) * direction
-fish.dataset.timer = Math.random() * 40 + 30
-console.log(fish.dataset.timer)
-console.log(fish.dataset.momentum)
+Fish.dataset.momentum = (Math.random() * FishStats.speedran + FishStats.speedbase) * direction
+Fish.dataset.timer = Math.random() * FishStats.timeran + FishStats.timebase
 }
 
-let momentum = Number(fish.dataset.momentum)
 
-fish.dataset.momentum = Number(fish.dataset.momentum) * 0.97
+Fish.dataset.momentum = Number(Fish.dataset.momentum) * 0.97
 
-let fishY = parseInt(fish.style.bottom) || 0
+let momentum = Number(Fish.dataset.momentum)
 
-let newfishY = fishY + momentum
-newfishY = Math.max(fishY + momentum, 0)
-newfishY = Math.min(fishY + Fishheight, 600)
+let fishY = parseInt(Fish.style.bottom) || 0
 
-fish.style.bottom = newfishY + 'px'
+fishY = fishY + momentum
 
-console.log(fishY)
+fishY = Math.max(fishY, 0)
+fishY = Math.min(fishY, 600 - Fishheight)
 
-fish.dataset.timer--;
+fish.style.bottom = fishY + 'px'
 
-
-}, 20)
+Fish.dataset.timer--;
+}
