27ede5baa36d74bff1a671d42eeb8df0a93588f4
thomas <hopsread@gmail.com>
2025-12-10 19:47:34 -0500

    figured out the idea for the gamemode

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+document.addEventListener("mousemove", function(event) {
+    const follower = document.getElementById("follower");
+    follower.style.left = event.clientX + "px";
+    follower.style.left = event.clientY + "px";
+});
+
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "ArrowRight") {
@@ -15,4 +21,4 @@ document.addEventListener("keydown", function(event) {
 
 
 
-})
+});
